4bb72dcaf97f0527923fb176287c8476897a0b1b
Taynara <montenegro.taynara@gmail.com>
2022-08-31 13:49:40 -0300

    Exercícios Strings

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,28 @@
+//Observe a string abaixo.
+
+//const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+//A partir dela, execute os passos abaixo:
+
+//a) Remova o excesso de espaços no final da string;
+
+//b) exiba, em um console.log() a quantidade de caracteres da string, antes e depois da remoção dos espaços;
+
+//c) Substitua os traços ________ por “sticioso”.
+
+
+
+
+
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+
+const novaString = minhaString.trim()
+
+console.log(minhaString)
+console.log(minhaString.length)
+
+
+
+console.log("Eu não sou supersticioso, mas sou um pouco sticioso.")
+console.log(novaString.length)
+
+
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,40 @@
+//A partir da frase a seguir, faça o que se pede
+
+//Jorge tem uma casa verde e com portão azul, com os dizeres: "BOAS VINDAS, mas não deixe o gato sair"
+//a) Crie uma const no seu código para guardar a frase (com aspas e tudo);
+
+//b) Crie uma nova string a partir da primeira, trocando verde por rosa, e azul por laranja;
+
+//c) Verifique se a nova string inclui verde, e se inclui laranja.
+
+//EXTRA: tente fazer o “mas não deixe o gato sair” ficar em maiúsculo, assim como o “BOAS VINDAS”
+
+
+
 //Crie a const para a frase aqui
+
+
+const frase = `Jorge tem uma casa verde e com portão azul, com os dizeres: "BOAS VINDAS, mas não deixe o gato sair.`
+console.log(frase)
+
+const primeiraAlteracao = frase.replaceAll("verde", "rosa").replaceAll("azul", "laranja")
+console.log(primeiraAlteracao)
+
+
+
+
+const segundaAlteracao = `Jorge tem uma casa rosa e com portão laranja, com os dizeres: "BOAS VINDAS,`
+
+const citacao = "mas não deixa o gato sair."
+
+
+const fraseMaiuscula = citacao.toUpperCase()
+
+console.log(segundaAlteracao, fraseMaiuscula)
+
+
+
+
+
+
+
